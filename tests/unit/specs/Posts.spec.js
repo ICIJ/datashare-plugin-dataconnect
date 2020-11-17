a5d860b8198bbc67e05c33ff6a1d9f611ed4d9ce
Madeline OLeary <madeline.w.oleary@gmail.com>
2020-11-17 10:16:36 +0100

    tests

--- tests/unit/specs/Posts.spec.js
+++ tests/unit/specs/Posts.spec.js
@@ -8,12 +8,48 @@ Vue.use(Vuex)
 jest.mock('axios')
 
 describe('Posts.vue', () => {
-  // Inspect the raw component options
-  it('should perform a get request for a topic', () => {
-    const state = { document: { idAndRouting: { id: 1 } }, search: { index: "test-datashare" } }
-    const store = new Vuex.Store({state})
-    const wrapper = shallowMount(Posts, {store})
+  const state = { document: { idAndRouting: { id: 1 } }, search: { index: "test-datashare" } }
+  const store = new Vuex.Store({state})
+  const wrapper = shallowMount(Posts, {store})
+
+  // afterEach(() => {
+  //   axios.request.mockClear()
+  // })
+
+  it('performs a get request for a topic in order to set the posts', () => {
     expect(axios.get).toBeCalled()
+  })
+
+  // console.log(wrapper.vm._data.posts)
+  it('successfully returns a data object from the Discourse API', async () => {
+    const data = {
+      data: {
+        post_stream: {
+          posts: [
+            {
+              id: 1,
+              username: "testuser",
+              created_at: "2020-10-16T15:21:29.039Z",
+              cooked: "<p>trying to see if it creates a topic</p>"
+            },
+            {
+              id: 2,
+              username: "testuser2",
+              created_at: "2020-10-16T17:00:29.039Z",
+              cooked: "<p>trying to see if it creates a second topic</p>"
+            }
+          ]
+        }
+      }
+    }
+
+    axios.get.mockImplementationOnce(() => Promise.resolve(data))
+
+    // await expect(received).resolves.toEqual(data)
+  })
+
+  it('erroneously returns a data object from the Discourse API', () => {
+
   })
 
 
