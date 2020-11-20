0b67cc9b8c6dfc627888763fda2034792401f9a8
Madeline OLeary <madeline.w.oleary@gmail.com>
2020-11-20 15:54:26 +0100

    recontextualizing

--- tests/unit/specs/Posts.spec.js
+++ tests/unit/specs/Posts.spec.js
@@ -37,7 +37,6 @@ describe('Posts.vue', () => {
   afterAll(() => jest.unmock('axios'))
 
   describe('When Posts loads the posts from the Discourse API on mounted', () => {
-
     describe('When Posts successfully performs a get request for a topic to retrieve posts', () => {
 
       beforeEach(() => {
@@ -74,71 +73,11 @@ describe('Posts.vue', () => {
         expect(wrapper.vm.posts).toHaveLength(0)
       })
     })
-
   })
 
   describe('When Posts creates a comment', () => {
-    describe('getCategory() - Get category from the Discourse API', () => {
-      const categoriesData = {
-        category_list: {
-          categories: [
-            {
-              id: 1,
-              permission: 1,
-              name: "Datashare Documents for test-datashare",
-              created_by_dataconnect: true,
-              icij_projects_for_category: [
-                {
-                  permission_type: 1,
-                  group_name: "test-datashare"
-                }
-              ]
-            }
-          ]
-        },
-        post_stream: {
-          posts: []
-        }
-      }
-
-      beforeEach(() => {
-        axios.get.mockResolvedValue({ data: categoriesData })
-      })
-
-      it('finds an existing category for the relevant project', async () => {
-        wrapper = await shallowMount(Posts, {store})
-        let response = await wrapper.vm.getCategory()
-
-        expect(axios.get).toBeCalled()
-        expect(response).toEqual(categoriesData.category_list.categories[0])
-      })
-    })
-
-    describe('createCategory() - Uses Discourse API to create a category', () => {
-      const returnData = {
-        id: 1,
-        name: 'Datashare Documents for test-datashare',
-        post_stream: {
-          posts: []
-        }
-      }
-
-      beforeEach(() => {
-        axios.post.mockResolvedValueOnce({ data: returnData })
-      })
-
-      it('Creates a category', async () => {
-        wrapper = await shallowMount(Posts, {store})
-
-        let response = await wrapper.vm.createCategory()
-
-        expect(axios.post).toBeCalled()
-        expect(response).toEqual(returnData)
-      })
-    })
-
-    describe('setCategory() - Only creates a category if it does not exist already', () => {
-      describe('Category exists', () => {
+    describe('Determining the Category in which to locate the Topic, where the comment will be stored', () => {
+      describe('getCategory() - Get category from the Discourse API', () => {
         const categoriesData = {
           category_list: {
             categories: [
@@ -162,45 +101,111 @@ describe('Posts.vue', () => {
         }
 
         beforeEach(() => {
-          axios.get.mockResolvedValueOnce({ data: categoriesData })
+          axios.get.mockResolvedValue({ data: categoriesData })
         })
 
-        afterEach(() => jest.unmock('axios'))
-
-        it('Returns the category from the Discourse API', async () => {
+        it('finds an existing category for the relevant project', async () => {
           wrapper = await shallowMount(Posts, {store})
-          let response = await wrapper.vm.setCategory()
+          let response = await wrapper.vm.getCategory()
 
           expect(axios.get).toBeCalled()
           expect(response).toEqual(categoriesData.category_list.categories[0])
         })
       })
 
-      describe('Category does not exist', () => {
+      describe('createCategory() - Uses Discourse API to create a category', () => {
         const returnData = {
-          id: 2,
+          id: 1,
           name: 'Datashare Documents for test-datashare',
           post_stream: {
             posts: []
           }
         }
 
-        beforeAll(() => {
-          axios.get.mockResolvedValue({ data: returnData })
-          axios.post.mockResolvedValue({ data: returnData })
+        beforeEach(() => {
+          axios.post.mockResolvedValueOnce({ data: returnData })
         })
 
-        it('creates the category', async () => {
+        it('Creates a category', async () => {
           wrapper = await shallowMount(Posts, {store})
-          const mockMethod = jest.fn().mockReturnValue(null)
-          wrapper.vm.getCategory = mockMethod
-          let response = await wrapper.vm.setCategory()
 
-          expect(mockMethod).toBeCalled()
+          let response = await wrapper.vm.createCategory()
+
           expect(axios.post).toBeCalled()
           expect(response).toEqual(returnData)
         })
       })
+
+      describe('setCategory() - combines both getCategory() and createCategory(), only creating a category if it does not exist already', () => {
+        describe('Category exists', () => {
+          const categoriesData = {
+            category_list: {
+              categories: [
+                {
+                  id: 1,
+                  permission: 1,
+                  name: "Datashare Documents for test-datashare",
+                  created_by_dataconnect: true,
+                  icij_projects_for_category: [
+                    {
+                      permission_type: 1,
+                      group_name: "test-datashare"
+                    }
+                  ]
+                }
+              ]
+            },
+            post_stream: {
+              posts: []
+            }
+          }
+
+          beforeEach(() => {
+            axios.get.mockResolvedValueOnce({ data: categoriesData })
+          })
+
+          afterEach(() => jest.unmock('axios'))
+
+          it('Returns the category from the Discourse API', async () => {
+            wrapper = await shallowMount(Posts, {store})
+            let response = await wrapper.vm.setCategory()
+
+            expect(axios.get).toBeCalled()
+            expect(response).toEqual(categoriesData.category_list.categories[0])
+          })
+        })
+
+        describe('Category does not exist', () => {
+          const returnData = {
+            id: 2,
+            name: 'Datashare Documents for test-datashare',
+            post_stream: {
+              posts: []
+            }
+          }
+
+          beforeAll(() => {
+            axios.get.mockResolvedValue({ data: returnData })
+            axios.post.mockResolvedValue({ data: returnData })
+          })
+
+          it('creates the category', async () => {
+            wrapper = await shallowMount(Posts, {store})
+            const mockMethod = jest.fn().mockReturnValue(null)
+            wrapper.vm.getCategory = mockMethod
+            let response = await wrapper.vm.setCategory()
+
+            expect(mockMethod).toBeCalled()
+            expect(axios.post).toBeCalled()
+            expect(response).toEqual(returnData)
+          })
+        })
+      })
     })
+
+    describe('Determining the topic in which to make the comment', () => {
+
+    })
+
   })
 })
