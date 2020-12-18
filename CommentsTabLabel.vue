bbbcaa0783f40e122198f4727916be59a69955f5
Madeline OLeary <madeline.w.oleary@gmail.com>
2020-12-18 13:57:55 +0100

    changing underscores to dashes in the discourse api url

--- CommentsTabLabel.vue
+++ CommentsTabLabel.vue
@@ -49,7 +49,7 @@
       async getCount () {
         const project = this.project
         const documentId = this.documentId
-        const url = `/api/proxy/${project}/custom_fields_api/topics/${documentId}/posts_count.json`
+        const url = `/api/proxy/${project}/custom-fields-api/topics/${documentId}/posts_count.json`
         const response = await axios.request({ url, method: 'get' })
         const { data: { posts_count: count } } = response
         return count
