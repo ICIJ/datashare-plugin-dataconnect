7f194070b86a5c91bb3b52ea36e68c8bc7d8024e
Madeline OLeary <madeline.w.oleary@gmail.com>
2021-03-10 13:41:40 +0100

    found a more robust solution for casing so this is no longer needed

--- components/CommentsForm.vue
+++ components/CommentsForm.vue
@@ -52,13 +52,12 @@ export default {
       }
     },
     async createCategory () {
-      const project = this.project.charAt(0).toLowerCase() + this.project.slice(1)
       const data = {
         name: `Datashare Documents for ${this.project}`,
         color: 'BF1E2E',
         text_color: 'FFFFFF',
         permissions: {
-          [project]: 1
+          [this.project]: 1
         },
         custom_fields: {
           created_by_dataconnect: 'true'
