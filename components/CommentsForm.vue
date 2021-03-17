e289f5a1ef492f9fccd5428ee8fccaceac0672aa
Anne L'Hôte <anne.lhote@gmail.com>
2021-03-17 11:24:39 +0100

    [i18n] Rename button label

--- components/CommentsForm.vue
+++ components/CommentsForm.vue
@@ -7,7 +7,7 @@
           placeholder="Type your comment here."
       ></textarea>
       <button class="comments-form__form__button btn btn-primary mt-2" @click="createComment()">
-        Publish
+        Post
       </button>
     </div>
   </div>
