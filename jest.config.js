1c1d833d78084327e27e555a9458be655f90ccf0
Pierre Romera <hello@pirhoo.com>
2021-04-02 15:40:34 +0200

    fix: allow import of CSS from Jest

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,9 @@ module.exports = {
     // '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
     '^.+\\.jsx?$': 'babel-jest'
   },
+  moduleNameMapper: {
+    '\\.(css|less)$': '<rootDir>/tests/unit/__mocks__/styleMock.js',
+  },
   transformIgnorePatterns: [
     'node_modules/(?!(bootstrap-vue)/)'
   ],
