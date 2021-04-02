e88de0a4028c7a95f315fce9f747e8689bbad1e9
Pierre Romera <hello@pirhoo.com>
2021-04-02 18:10:46 +0200

    feat: add option to read port from argv

--- bs-config.js
+++ bs-config.js
@@ -1,5 +1,8 @@
 const { spawn } = require('child_process')
 const bs = require('browser-sync').create()
+// Read port from the first argument.
+// By default we use the 8888 port which is the one we use on Datashare devenv.
+const DATASHARE_PORT = process.argv.slice(2).pop() || 8888
 
 function yarn(opts = [], label = null) {
   if (label !== null) {
@@ -28,6 +31,6 @@ bs.init({
   ],
   ignore: ['tmp/**/*', 'dist/**/*'],
   open: false,
-  proxy: "localhost:8888",
+  proxy: `localhost:${DATASHARE_PORT}`,
   watch: true
 })
