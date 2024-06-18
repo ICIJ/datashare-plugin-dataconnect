59f97f8baa3944056000869b528acfd8ed9f28ea
Pierre Romera <hello@pirhoo.com>
2024-06-18 10:48:42 +0000

    test: migrate to vitest and vue 3 with core plugin

--- tests/unit/specs/components/Comments.spec.js
+++ tests/unit/specs/components/Comments.spec.js
@@ -1,57 +1,44 @@
-import { shallowMount, createLocalVue } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import axios from 'axios'
-import BootstrapVue from 'bootstrap-vue'
-import Vue from 'vue'
-import VueWait from 'vue-wait'
-import Vuex from 'vuex'
 
+import CorePlugin from '../../CorePlugin.js'
 import Comments from '@components/Comments.vue'
 
-const localVue = createLocalVue()
-localVue.use(BootstrapVue)
-localVue.use(VueWait)
-localVue.use(Vuex)
-
-jest.mock('axios')
+vi.mock('axios')
 
 describe('Comments.vue', () => {
-  const document = { doc: { slicedName: 'test.pdf' }, idAndRouting: { id: '1' } }
-  const state = { document, search: { index: 'test-datashare' } }
-  const store = new Vuex.Store({ state })
-  const wait = new VueWait()
-  let wrapper = null
-
+  let wrapper
+  
   beforeEach(async () => {
+    const { plugins, stubs } = CorePlugin.init()
     Comments.methods.getCount = () => 2
-    wrapper = await shallowMount(Comments, { store, localVue, wait })
-    axios.request.mockClear()
+    wrapper = await shallowMount(Comments, { global: { plugins, stubs } })
   })
 
-  afterAll(() => jest.unmock('axios'))
+  afterEach(() => {
+    axios.request.mockReset()
+  })
 
-  describe('getComments', () => {
-    it('should return empty array if an error occurs', async () => {
-      try {
-        axios.request.mockRejectedValue()
-      } catch (_) {  }
-      expect(wrapper.vm.comments).toHaveLength(0)
-      expect(wrapper.vm.comments).toEqual([])
-    })
+  it('should return empty array if an error occurs', async () => {
+    try {
+      axios.request.mockRejectedValue()
+    } catch (_) {  }
+    expect(wrapper.vm.comments).toHaveLength(0)
+    expect(wrapper.vm.comments).toEqual([])
+  })
 
-    it('should return empty array if no topics', async () => {
-      axios.request.mockResolvedValue({ data: { topic_view_posts: { post_stream: { posts: [] } } } })
-      await wrapper.vm.getComments()
-
-      expect(wrapper.vm.comments).toHaveLength(0)
-      expect(wrapper.vm.comments).toEqual([])
-    })
-
-    it('should retrieve topics if any', async () => {
-      axios.request.mockResolvedValue({ data: { topic_view_posts: { post_stream: { posts: [{ id: 1 }, { id: 2 }] } } } })
-      await wrapper.vm.getComments()
-
-      expect(wrapper.vm.comments).toHaveLength(2)
-      expect(wrapper.vm.comments).toEqual([{ id: 1 }, { id: 2 }])
-    })
+  it('should return empty array if no topics', async () => {
+    axios.request.mockResolvedValue({ data: { topic_view_posts: { post_stream: { posts: [] } } } })
+    
+    expect(wrapper.vm.comments).toHaveLength(0)
+    expect(wrapper.vm.comments).toEqual([])
+  })
+  
+  it('should retrieve topics if any', async () => {
+    axios.request.mockResolvedValue({ data: { topic_view_posts: { post_stream: { posts: [{ id: 1 }, { id: 2 }] } } } })
+    await wrapper.vm.getComments()
+      
+    expect(wrapper.vm.comments).toHaveLength(2)
+    expect(wrapper.vm.comments).toEqual([{ id: 1 }, { id: 2 }])
   })
 })
