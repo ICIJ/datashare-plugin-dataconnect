b37588ab079d9f65ac124a83e3a9316b3347fb2c
Pierre Romera <hello@pirhoo.com>
2024-06-18 11:01:03 +0000

    test: migrate to vitest and vue 3 with core plugin

--- tests/unit/specs/components/CommentsTabLabel.spec.js
+++ tests/unit/specs/components/CommentsTabLabel.spec.js
@@ -1,44 +1,53 @@
-import { shallowMount, createLocalVue } from '@vue/test-utils'
+import { flushPromises, shallowMount } from '@vue/test-utils'
 import axios from 'axios'
-import Vue from 'vue'
-import Vuex from 'vuex'
 
+import CorePlugin from '../../CorePlugin.js'
 import CommentsTabLabel from '@components/CommentsTabLabel.vue'
 
-const localVue = createLocalVue()
-localVue.use(Vuex)
-
-jest.mock('axios')
+vi.mock('axios')
 
 describe('CommentsTabLabel.vue', () => {
-  const state = { document: { doc: { slicedName: 'test.pdf', id: '1' } }, search: { index: 'test-datashare' } }
-  const store = new Vuex.Store({ state })
-  const flushPromises = () => new Promise(resolve => setTimeout(resolve, 0))
+  let wrapper
+  
+  describe('with no comments', () => {
 
-  beforeEach(async () => {
-    axios.request.mockClear()
+    beforeEach(async () => {
+      const { plugins, stubs } = CorePlugin.init()
+      const data = { "posts_count": 0 }
+      axios.request.mockResolvedValue({ data })    
+      wrapper = shallowMount(CommentsTabLabel, { global: { plugins, stubs } })
+      await flushPromises()
+    })
+
+    afterEach(() => {
+      axios.request.mockReset()
+    })
+
+    it('shows the "Comments" label', () => {    
+      expect(wrapper.find('.comments-tab-label__label').text()).toBe("Comments")
+    })
+
+    it('doesnt show 0 comments in a badge', () => {
+      expect(wrapper.find('.comments-tab-label__count').exists()).toBeFalsy()
+    })
   })
 
-  afterAll(() => jest.unmock('axios'))
+  describe('with no comments', () => {
 
-  it('shows the "Comments" label', () => {
-    const wrapper = shallowMount(CommentsTabLabel, { localVue, store })
-    expect(wrapper.find('.comments-tab-label__label').text()).toBe("Comments")
-  })
+    beforeEach(async () => {
+      const { plugins, stubs } = CorePlugin.init()
+      const data = { "posts_count": 10 }
+      axios.request.mockResolvedValue({ data })
+      wrapper = shallowMount(CommentsTabLabel, { global: { plugins, stubs } })
+      await flushPromises()
+    })
 
-  it('doesnt show 0 comments in a badge', async () => {
-    const data = { "posts_count": 0 }
-    axios.request.mockResolvedValue({ data })
-    const wrapper = shallowMount(CommentsTabLabel, { localVue, store })
-    await flushPromises()
-    expect(wrapper.find('.comments-tab-label__count').exists()).toBeFalsy()
-  })
+    afterEach(() => {
+      axios.request.mockReset()
+    })
 
-  it('show 10 comments in a badge', async () => {
-    const data = { "posts_count": 10 }
-    axios.request.mockResolvedValue({ data })
-    const wrapper = shallowMount(CommentsTabLabel, { localVue, store })
-    await flushPromises()
-    expect(wrapper.find('.comments-tab-label__count').text()).toBe("10")
+    it('show 10 comments in a badge', () => { 
+      expect(wrapper.find('.comments-tab-label__count').text()).toBe("10")
+    })
   })
 })
